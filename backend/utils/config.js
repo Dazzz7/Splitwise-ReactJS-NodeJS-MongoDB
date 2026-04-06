f4906aed17a2fa32e55df4bf4a8503c84c633f2e
Darshana <darshana.choudhary.000@gmail.com>
2026-04-06 02:09:41 -0700

    Update config.js

---
+++ backend/utils/config.js
@@ -0,0 +1,10 @@
+"use strict";
+const config = {
+    secret: "cmpe273_kafka_passport_mongo",
+    frontendURI: "http://localhost:3000",
+    kafkaURI: "localhost:2181",
+    mongoDBURI:
+        "",
+};
+
+module.exports = config;
---
+++ backend/utils/constants.js
@@ -0,0 +1,18 @@
+"use strict";
+module.exports = {
+    STATUS_CODE: {
+        SUCCESS: 200,
+        NOT_SUCCESS: 201,
+        BAD_REQUEST: 400,
+        UNAUTHORIZED: 401,
+        INTERNAL_SERVER_ERROR: 500,
+    },
+    MESSAGES: {
+        INVALID_CREDENTIALS: "Invalid Credentials",
+        INTERNAL_SERVER_ERROR: "Internal Server Error",
+        SUCCESS: "Successful",
+        INVALID_STATE: "State Name is not valid",
+        DATA_NOT_FOUND: "Data doesn't Exists",
+        USER_ALREADY_EXISTS: "User Already Exists",
+    },
+};
